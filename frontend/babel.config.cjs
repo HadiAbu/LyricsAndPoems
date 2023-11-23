8588693aa94bb0f9a96bd6ca41f9ff12f461dfe0
HadiAbu <hadi.abuhamed@gmail.com>
2023-11-23 13:11:53 +0000

    added tests and fixed configurations

--- frontend/babel.config.cjs
+++ frontend/babel.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    ["@babel/preset-react", { targets: { node: "current" } }], // add this
+    ["@babel/preset-env", { targets: { node: true } }],
+    ["@babel/preset-react", { targets: { node: true } }], // add this
+  ],
+  plugins: [
+    "@babel/plugin-syntax-jsx",
+    "@babel/plugin-proposal-class-properties",
   ],
 };
--- frontend/src/__tests__/First.test.js
+++ frontend/src/__tests__/First.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 import React from "react";
 import { render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import App from "../App";
 
 describe("first test here", () => {
@@ -14,6 +14,7 @@ describe("first test here", () => {
     expect(5).toBe(5);
   });
   test("find button and click it", () => {
-    screen.getByRole("button", { name: /count is 0/i });
+    userEvent.click(screen.findByRole("button", { name: /count is 0/i }));
+    screen.findByRole("button", { name: /count is 1/i });
   });
 });
