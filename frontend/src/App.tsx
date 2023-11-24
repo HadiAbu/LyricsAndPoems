c8e2eddb51638408904b1ed3c150312a5f724473
HadiAbu <hadi.abuhamed@gmail.com>
2023-11-24 11:42:20 +0000

    fixed the selection test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { useState } from "react";
 import "./App.css";
 
+const attributes = [
+  { key: 1, label: "tacos" },
+  { key: 2, label: "burgers" },
+  { key: 3, label: "pizzas" },
+];
+
 const App = () => {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Vite + React</h1>
@@ -14,6 +19,22 @@ const App = () => {
         </button>
         <input type="text" placeholder="talk dirty to me" />
         <input type="checkbox" />
+      </div>
+      <div className="card">
+        <select data-testid="select">
+          <option value="default">Make your choice</option>
+          {attributes.map((item) => {
+            return (
+              <option
+                data-testid="select-option"
+                key={item.key}
+                value={item.key}
+              >
+                {item.label}
+              </option>
+            );
+          })}
+        </select>
       </div>
     </>
   );
--- frontend/src/__tests__/First.test.js
+++ frontend/src/__tests__/First.test.js
@@ -4,7 +4,7 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
 
-describe("first test here", () => {
+describe("First test suite to cover many scenarios for reference", () => {
   test("first basic test", () => {
     render(<App />);
     expect(5).not.toBe(6);
@@ -19,8 +19,8 @@ describe("first test here", () => {
 
     let button = await screen.findByRole("button");
     expect(button.textContent).toBe("count is 0");
-    user.click(button);
-    user.click(button);
+    await user.click(button);
+    await user.click(button);
     expect(await screen.findByText("count is 2")).toBeDefined();
   });
   test("find button and click it with fireEvent", async () => {
@@ -45,11 +45,22 @@ describe("first test here", () => {
   });
   test("make sure a checkbox is checked", async () => {
     render(<App />);
+    const user = userEvent.setup();
 
     let checkbox = await screen.findByRole("checkbox");
     expect(checkbox.checked).toBe(false);
-    fireEvent.click(checkbox);
+    // fireEvent.click(checkbox);
+    await user.click(checkbox);
 
     expect(checkbox.checked).toBe(true);
+  });
+  test("Selection", async () => {
+    const { getByTestId, getAllByTestId } = render(<App />);
+
+    fireEvent.change(getByTestId("select"), { target: { value: 2 } });
+    let options = getAllByTestId("select-option");
+    expect(options[0].selected).toBeFalsy();
+    expect(options[1].selected).toBeTruthy();
+    expect(options[2].selected).toBeFalsy();
   });
 });
